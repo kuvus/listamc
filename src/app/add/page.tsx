cd7eb67106c36f69aab5c5d8f60de43de05561e0
kuvuś <kuba17ism@tuta.io>
2023-09-08 23:34:18 +0200

    Update server adding steps, improve error handling

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -8,50 +8,75 @@ import * as process from 'process'
 
 export default function Add() {
     const [step, setStep] = useState(0)
-
+    const [addStep, setAddStep] = useState(0) // State for the second step step
     const [serverAddress, setServerAddress] = useState('')
+    const [stepFail, setStepFail] = useState(-1)
+    const [nextStepReady, setNextStepReady] = useState(true)
 
     const inputRef = useRef<HTMLInputElement>(null)
 
     const steps = [
         <StepOne key={'step-1'} ref={inputRef} />,
-        <StepTwo key={'step-2'} step={step} />,
+        <StepTwo key={'step-2'} step={addStep} error={stepFail} />,
         <StepThree key={'step-3'} />,
     ]
 
     const incrementStep = () => {
+        setStepFail(-1)
         if (step == 0) setServerAddress(inputRef.current?.value || '')
         setStep(cs => (cs == steps.length - 1 ? cs : cs + 1))
     }
 
     const decrementStep = () => {
+        setStepFail(-1)
         setStep(cs => (cs == 0 ? cs : cs - 1))
     }
 
     const fetchServerDetails = async (address: string) => {
+        console.log('a')
         const gamedataURL = encodeURI(
             `https://api.gamedata.okaeri.cloud/v1/minecraftjava/${address.replaceAll(
                 '/',
                 ''
             )}/info`
         )
-        const res = await fetch(gamedataURL)
+        console.log(gamedataURL)
+        try {
+            const res = await fetch(gamedataURL, {
+                mode: 'no-cors',
+            })
 
-        if (!res.ok) {
-            return { error: true }
+            if (!res.ok) {
+                setStepFail(0)
+                return { error: true }
+            }
+
+            console.log(await res.json())
+
+            return res.json()
+        } catch (e) {
+            setStepFail(0)
+            console.log(e)
         }
 
-        return res.json()
+        return { error: true }
     }
 
     const checkIfServerInDB = async (address: string) => {
-        const res = await fetch(
-            `${process.env.NEXT_PUBLIC_API_URL}/api/server/${address}`
-        )
-        if (!res.ok) {
-            return { error: true }
+        try {
+            const res = await fetch(
+                `${process.env.NEXT_PUBLIC_API_URL}/api/server/${address}`
+            )
+            if (!res.ok) {
+                setStepFail(1)
+                return { error: true }
+            }
+            return res.json()
+        } catch (e) {
+            setStepFail(1)
         }
-        return res.json()
+
+        return { error: true }
     }
 
     useEffect(() => {
@@ -64,9 +89,8 @@ export default function Add() {
                 break
             case 1:
                 ;(async () => {
-                    const serverDetails = await fetchServerDetails(
-                        serverAddress
-                    )
+                    const serverDetails =
+                        await fetchServerDetails(serverAddress)
 
                     if (serverDetails.error) {
                         console.log('error')
@@ -88,6 +112,8 @@ export default function Add() {
         }
     }, [step, serverAddress])
 
+    console.log(stepFail)
+
     return (
         <section className={'container mt-16 flex flex-col gap-4 xl:max-w-7xl'}>
             <div className={'grid grid-cols-3 gap-4'}>
@@ -107,12 +133,14 @@ export default function Add() {
                         onClick={decrementStep}>
                         Cofnij
                     </Button>
-                    <Button
-                        styling={'primary'}
-                        element={'button'}
-                        onClick={incrementStep}>
-                        Przejdź do kolejnego kroku!
-                    </Button>
+                    {stepFail < 0 && (
+                        <Button
+                            styling={'primary'}
+                            element={'button'}
+                            onClick={incrementStep}>
+                            Przejdź do kolejnego kroku!
+                        </Button>
+                    )}
                 </div>
             </div>
         </section>
--- src/components/add/Steps.tsx
+++ src/components/add/Steps.tsx
@@ -1,6 +1,6 @@
-'use client'
-
-import { forwardRef, useState } from 'react'
+import { forwardRef, FunctionComponent } from 'react'
+import { Loader2, X, Check } from 'lucide-react'
+import { Alert } from '@/components/shared/Alert'
 
 const StepOneDef = (props, ref) => {
     return (
@@ -20,7 +20,43 @@ const StepOneDef = (props, ref) => {
 
 const StepOne = forwardRef(StepOneDef)
 
-const StepTwo = ({ step, key }) => {
+type StateIconType = {
+    state: 'load' | 'ok' | 'fail'
+}
+
+const StateIcon: FunctionComponent<StateIconType> = ({ state }) => {
+    switch (state) {
+        case 'load':
+            return <Loader2 className={'animate-spin text-text'} />
+        case 'ok':
+            return <Check className={'text-green-500'} />
+        case 'fail':
+            return <X className={'text-red-600'} />
+        default:
+            return null
+    }
+}
+
+type StepStateIconType = {
+    step: number
+    current: number
+    error: number
+}
+
+const StepStateIcon: FunctionComponent<StepStateIconType> = ({
+    step,
+    current,
+    error,
+}) => {
+    console.log(step, current, error, step === current, error === current)
+    if (error === step) return <StateIcon state={'fail'} />
+    if (current > step) return <StateIcon state={'ok'} />
+    if (current <= step) return <StateIcon state={'load'} />
+
+    return null
+}
+
+const StepTwo = ({ step, key, error }) => {
     // TODO:
     // 1. Fetch do API Gamedata
     // 2. Fetch do API listy czy nie ma jeszcze serwera na liście
@@ -28,16 +64,37 @@ const StepTwo = ({ step, key }) => {
     // 3b. Jeśli jest to wyświetlamy komunikat o tym
     // 4. Przejście do kroku 3
 
+    // console.log(error)
+
+    //
+    // console.log(step, error)
+    const errorText = [
+        'Wystąpił błąd podczas pobierania danych serwera',
+        'Wystąpił błąd podczas pobierania danych serwera',
+    ]
+
     return (
         <>
             <h1 className={'text-2xl'}>Weryfikowanie statusu serwera...</h1>
-            <span>Sprawdzanie statusu serwera...</span>
-            <span className={step > 0 ? '' : 'hidden'}>
-                Sprawdzanie czy serwer został już dodany na listę...
-            </span>
-            <span className={step > 1 ? '' : 'hidden'}>
-                Dodawanie serwera na listę...
-            </span>
+            {error >= 0 && <Alert type={'error'}>{errorText[error]}</Alert>}
+            <div className={'flex gap-2'}>
+                <StepStateIcon step={0} current={step} error={error} />
+                <span>Sprawdzanie statusu serwera...</span>
+            </div>
+            {step > 0 && (
+                <div className={'flex gap-2'}>
+                    <StepStateIcon step={1} current={step} error={error} />
+                    <span className={'flex gap-2'}>
+                        Sprawdzanie czy serwer został już dodany na listę...
+                    </span>
+                </div>
+            )}
+            {step > 1 && (
+                <div className={'flex gap-2'}>
+                    <StepStateIcon step={2} current={step} error={error} />
+                    <span>Dodawanie serwera na listę...</span>
+                </div>
+            )}
         </>
     )
 }
