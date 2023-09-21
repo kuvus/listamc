7eadbeea091e87382c9fa54c4e5d2eef880165bd
kuvuś <kuba17ism@tuta.io>
2023-09-22 00:32:55 +0200

    Update packages, add logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,27 @@
+import pino from 'pino'
+import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
+
+const stream = createWriteStream({
+    apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY || '',
+    sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN || '',
+})
+
+const send = createPinoBrowserSend({
+    apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY || '',
+    sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN || '',
+})
+
+const logger = pino(
+    {
+        browser: {
+            transmit: {
+                level: 'info',
+                send: send,
+            },
+        },
+        level: 'debug',
+    },
+    stream
+)
+
+export default logger
