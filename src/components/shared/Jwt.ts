7052cd9ab456cdc1d5d14e65b3ae5dd2d4a4a608
kuvuś <kuba17ism@tuta.io>
2023-11-22 22:26:59 +0100

    Make JWT function server-only

--- src/components/shared/Jwt.ts
+++ src/components/shared/Jwt.ts
@@ -2,6 +2,7 @@ import 'server-only'
 import { sign } from 'jsonwebtoken'
 
 export default function jwt(obj: any) {
+    'use server-only'
     return sign(obj, process.env.JWT_SECRET, {
         expiresIn: '15 minutes',
     })
