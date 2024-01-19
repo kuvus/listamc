45e8a7e398d704489de7f26938321c0c0135cb84
kuvuś <kuba17ism@tuta.io>
2024-01-19 23:10:09 +0100

    Fix next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverActions: true,
-    },
     typescript: {
         ignoreBuildErrors: true, // TODO: Remove after next-auth fix
     },
