a41488513fe8acbed8c3b40b5d6ed99f84a51276
kuvuś <kuba17ism@tuta.io>
2024-02-06 23:57:59 +0100

    Fix widgets styling

--- src/components/server/WidgetsModal.tsx
+++ src/components/server/WidgetsModal.tsx
@@ -10,10 +10,15 @@ export const WidgetsModal = ({ serverId }) => {
                 <iframe
                     src={`${process.env.NEXT_PUBLIC_PAGE_URL}/widget/${serverId}?text=%23ffffff`}
                     width={'100%'}
-                    height={'80px'}></iframe>
+                    height={'82px'}
+                    className={'rounded-md border border-semi-border'}></iframe>
+                <span>Kod</span>
                 <textarea
-                    value={`<iframe src="${process.env.NEXT_PUBLIC_PAGE_URL}/widget/${serverId}" width="100%" height="80"></iframe>`}
+                    value={`<iframe src='${process.env.NEXT_PUBLIC_PAGE_URL}/widget/${serverId}' width='100%' height='80'></iframe>`}
                     readOnly={true}
+                    className={
+                        'w-full rounded-md border border-semi-border bg-bg-800 p-2'
+                    }
                 />
             </Modal>
         </Suspense>
