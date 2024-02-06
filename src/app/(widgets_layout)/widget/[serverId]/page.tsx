a25bf07a19df4fd377f827dbad588fe667303e79
kuvuś <kuba17ism@tuta.io>
2024-02-06 01:17:14 +0100

    Update widgets page

--- src/app/(widgets_layout)/widget/[serverId]/page.tsx
+++ src/app/(widgets_layout)/widget/[serverId]/page.tsx
@@ -1,18 +1,20 @@
 import { StatusIndicator } from '@/components/list/StatusIndicator'
 import { Users } from 'lucide-react'
+import { getServer } from '@/data/servers/getServer'
 
 export default async function Widget({ params: { serverId }, searchParams }) {
-    const data = await getData(serverId)
+    const data = await getServer(serverId)
 
-    if (data.error)
+    if (!data || !data.ServerData)
         return <div>Wystąpił błąd podczas pobierania informacji.</div>
-
-    console.log('SP: ', searchParams)
 
     return (
         <div
-            className={`flex h-full w-full flex-row justify-between gap-4 p-2  text-[#ffffff]`}>
-            <img src={data.ServerData.icon} alt='' />
+            className={`flex h-full w-full flex-row justify-between gap-4 bg-bg-800 p-2 text-text`}>
+            <div className={'flex flex-row items-center gap-4'}>
+                <img src={data.ServerData.icon} alt='' />
+                <div className={'text-xl font-semibold'}>{data.address}</div>
+            </div>
             <div className={'flex flex-col items-end justify-center gap-0'}>
                 <div className={'flex items-center gap-2'}>
                     <span>{data.ServerData.version}</span>
@@ -28,16 +30,4 @@ export default async function Widget({ params: { serverId }, searchParams }) {
             </div>
         </div>
     )
-}
-
-const getData = async (id: string) => {
-    const res = await fetch(`${process.env.API_URL}/servers/${id}`, {
-        next: { revalidate: 30 },
-    })
-
-    if (!res.ok) {
-        return { error: true }
-    }
-
-    return res.json()
 }
